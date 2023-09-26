e0d33d25d039db3d17a1d2b5e439eac82bb0db27
Ryan-Thrall <ryanthrall04@gmail.com>
2023-09-26 11:52:37 -0600

    Setup Front and Back End

--- server/index.js
+++ server/index.js
@@ -1,2 +1,7 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+
+app.listen(3002, () => {
+  console.log('Server running on port 3002');
+});
